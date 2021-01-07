d53137506d2ef5fc42d1821bc242b4b1137c06f6
Will-Helliwell <wj.helliwell@gmail.com>
2021-01-07 18:14:52 +0000

    fun merge party conflicts resolved

--- spec/features/listings_spec.rb
+++ spec/features/listings_spec.rb
@@ -1,8 +1,12 @@
 feature "/listings" do
   it "allows each listing to be booked" do
+    login
     visit('/listings/new')
     add_listing1
     add_listing2
+    fill_in("available_night", with: "01/01/2021")
+    click_button("Add date")
+    click_button("Finish")
     within("//li[@id='1']") do
       click_button('Request to book')
     end
