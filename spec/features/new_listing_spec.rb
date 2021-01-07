158b7b34eecbabf4ffd53ff5cbbc870f057d45ba
xavierloos <xavier.loos04@gmail.com>
2021-01-07 17:45:54 +0000

    Update rspec

--- spec/features/new_listing_spec.rb
+++ spec/features/new_listing_spec.rb
@@ -1,13 +1,12 @@
 feature "new listing" do
-
   scenario "adding a new listing that is then visible" do
     login
     add_listing1
     click_button("Finish")
-    expect(page).to have_content 'testtitle'
-    expect(page).to have_content 'test description'
-    expect(page).to have_content '100'
-    expect(page).not_to have_content 'sinatra'
+    expect(page).to have_content "testtitle"
+    expect(page).to have_content "test description"
+    expect(page).to have_content "100"
+    expect(page).not_to have_content "sinatra"
   end
 
   scenario "can add multiple listings" do
@@ -16,20 +15,20 @@ feature "new listing" do
     click_button("Finish")
     add_listing2
     click_button("Finish")
-    expect(page).to have_content 'testtitle'
-    expect(page).to have_content 'testtitle2'
-    expect(page).not_to have_content 'sinatra'
+    expect(page).to have_content "testtitle"
+    expect(page).to have_content "testtitle2"
+    expect(page).not_to have_content "sinatra"
   end
 
   scenario "can add available dates" do
+    login
     add_listing1
     fill_in("available_night", with: "01/01/2021")
-    click_button('Add date')
+    click_button("Add date")
     click_button("Finish")
     click_link("testtitle")
-    expect(page).to have_content 'testtitle'
-    expect(page).to have_content '01/01/2021'
-    expect(page).not_to have_content 'sinatra'
+    expect(page).to have_content "testtitle"
+    expect(page).to have_content "01/01/2021"
+    expect(page).not_to have_content "sinatra"
   end
-
 end
