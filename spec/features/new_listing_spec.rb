5d972201142e7809a9e0f6cba5a190a8af5c7ffa
xavierloos <xavier.loos04@gmail.com>
2021-01-07 14:54:40 +0000

    RSpec

--- spec/features/new_listing_spec.rb
+++ spec/features/new_listing_spec.rb
@@ -1,5 +1,6 @@
 feature "new listing" do
   scenario "adding a new listing that is then visible" do
+    login
     add_listing1
     expect(page).to have_content 'testtitle'
     expect(page).to have_content 'test description'
@@ -7,6 +8,7 @@ feature "new listing" do
     expect(page).not_to have_content 'sinatra'
   end
   scenario "can add multiple listings" do
+    login
     add_listing1
     add_listing2
     expect(page).to have_content 'testtitle'
